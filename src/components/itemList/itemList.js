c492c1132cc68abfe3d88b4ef3a8d3b3c88aa910
Andrey <2499931@gmail.com>
2021-08-10 16:13:08 +0400

    добавил хуки

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,66 +1,151 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./itemList.css";
 import Spinner from "../spinner";
-import gotService from "../../services/gotService";
-import PropTypes from "prop-types";
+// import gotService from "../../services/gotService";
+// import PropTypes from "prop-types";
 
-class ItemList extends Component {
-  static defaultProps = {
-    onItemSelected: () => {},
-  };
+function ItemList({ getData, onItemSelected, renderItem }) {
+  const [itemList, updateList] = useState([]);
 
-  static propTypes = {
-    onItemSelected: PropTypes.func,
-  };
+  useEffect(() => {
+    getData().then((data) => {
+      updateList(data);
+    });
+  }, []);
 
-  renderItems(arr) {
-    return arr.map((item) => {
+  function renderItems(arr) {
+    return arr.map((item, i) => {
       const { id } = item;
 
-      const label = this.props.renderItem(item);
+      const label = renderItem(item);
 
       return (
-        <li
-          key={id}
-          className="item"
-          onClick={() => this.props.onItemSelected(id)}
-        >
+        <li key={id} className="item" onClick={() => onItemSelected(id)}>
           {label}
         </li>
       );
     });
   }
 
-  render() {
-    const { data } = this.props;
-    const items = this.renderItems(data);
-
-    return <ul>{items}</ul>;
+  if (!itemList) {
+    return <Spinner />;
   }
+
+  const items = renderItems(itemList);
+
+  return <ul>{items}</ul>;
 }
 
-const withData = (View, getData) => {
-  return class extends Component {
-    state = {
-      data: null,
-    };
+export default ItemList
 
-    componentDidMount() {
-      getData().then((data) => {
-        this.setState({ data });
-      });
-    }
+// ! классовый компонент
 
-    render() {
-      const { data } = this.state;
+// export default class ItemList extends Component {
+//   state = {
+//     itemList: null,
+//   };
 
-      if (!data) {
-        return <Spinner />;
-      }
-      return <View {...this.props} data={data} />;
-    }
-  };
-};
+//   renderItems(arr) {
+//     return arr.map((item, i) => {
+//       const { id } = item;
 
-const {getAllCharacters} = new gotService()
-export default withData(ItemList, getAllCharacters);
+//       const label = this.props.renderItem(item);
+
+//       return (
+//         <li
+//           key={id}
+//           className="item"
+//           onClick={() => this.props.onItemSelected(id)}
+//         >
+//           {label}
+//         </li>
+//       );
+//     });
+//   }
+
+//   render() {
+//     const { itemList } = this.state;
+
+//     if (!itemList) {
+//       return <Spinner />;
+//     }
+
+//     const items = this.renderItems(itemList);
+
+//     return <ul>{items}</ul>;
+//   }
+// }
+//   componentDidMount() {
+//     const { getData } = this.props;
+
+//     getData().then((itemList) => {
+//       this.setState({ itemList });
+//     });
+//   }
+
+// ItemList.defaultProps = {
+//   onItemSelected: () => {}
+// }
+
+// ! компонент высшего порядка
+
+// class ItemList extends Component {
+//   static defaultProps = {
+//     onItemSelected: () => {},
+//   };
+
+//   static propTypes = {
+//     onItemSelected: PropTypes.func,
+//   };
+
+//   renderItems(arr) {
+//     return arr.map((item) => {
+//       const { id } = item;
+
+//       const label = this.props.renderItem(item);
+
+//       return (
+//         <li
+//           key={id}
+//           className="item"
+//           onClick={() => this.props.onItemSelected(id)}
+//         >
+//           {label}
+//         </li>
+//       );
+//     });
+//   }
+
+//   render() {
+//     const { data } = this.props;
+//     const items = this.renderItems(data);
+
+//     return <ul>{items}</ul>;
+//   }
+// }
+
+// const withData = (View, getData) => {
+//   return class extends Component {
+//     state = {
+//       data: null,
+//     };
+
+//     componentDidMount() {
+//       getData().then((data) => {
+//         this.setState({ data });
+//       });
+//     }
+
+//     render() {
+//       const { data } = this.state;
+
+//       if (!data) {
+//         return <Spinner />;
+//       }
+//       return <View {...this.props} data={data} />;
+//     }
+//   };
+// };
+
+// const {getAllCharacters} = new gotService()
+// export default withData(ItemList, getAllCharacters);
